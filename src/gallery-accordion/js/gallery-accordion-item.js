ab4e8ecfc4a74c2ccd77db1575f3fc5df1db5181
ipeychev <iliyan.peychev@gmail.com>
2010-02-25 08:51:41 +0200

    AccordionItem - resize() function - documentation updated

--- src/gallery-accordion/js/gallery-accordion-item.js
+++ src/gallery-accordion/js/gallery-accordion-item.js
@@ -878,9 +878,10 @@ Y.extend( AccordionItem, Y.Widget, {
 
 
     /**
-     * Forces the item to update its size as result of content updating via 'innerHTML'
+     * Forces the item to resize as result of direct content manipulation (via 'innerHTML').
+     * This method should be invoked if 'contentHeight' property has been set to 'auto'.
      *
-     * @method refresh
+     * @method resize
      */
     resize : function(){
         this.fire( "contentUpdate" );
